20d54dfcb5929d7672c2786de6415c91fedccf36
JonhMatador <sesisamuel15@gmail.com>
2024-09-23 08:14:29 -0300

    Create main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "Pessoa.h"
+
+int main() {
+    Pessoa samuel;
+
+    std::cout << "Amount of balls: " << samuel.inferior.partes.qtdBalls << std::endl;
+
+    return 0;
+}
