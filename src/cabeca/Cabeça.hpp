0da45a1f4f3bce751f638291dc0b6142b29ac7f3
JonhMatador <sesisamuel15@gmail.com>
2024-09-23 08:24:52 -0300

    Create Cabeça.hpp

---
+++ src/cabeca/Cabeça.hpp
@@ -0,0 +1,17 @@
+#ifndef CABECA_H
+#define CABECA_H
+
+#include "Olhos.h"
+#include "Boca.h"
+#include "Orelha.h"
+
+class Cabeca {
+public:
+    Olhos olho;
+    Boca boca;
+    Orelha orelha;
+
+    Cabeca() {}
+};
+
+#endif
