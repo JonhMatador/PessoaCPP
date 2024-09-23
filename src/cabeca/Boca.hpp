9fe87b6e0fe18c1677a597d0012466e15425eda3
JonhMatador <sesisamuel15@gmail.com>
2024-09-23 08:23:19 -0300

    Create Boca.hpp

---
+++ src/cabeca/Boca.hpp
@@ -0,0 +1,14 @@
+#ifndef BOCA_H
+#define BOCA_H
+
+#include <string>
+
+class Boca {
+public:
+    std::string corDoLabios;
+    int qtdDentes;
+
+    Boca() : corDoLabios("Remelho"), qtdDentes(32) {}
+};
+
+#endif
